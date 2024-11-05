01a30e1e68b3c6b3f5671db6941246d06c1d3bc0
arkhanpstucse <ar.ap@nbr.gov.bd>
2024-11-05 16:17:23 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PracticeGitIgnore {
     public static void main(String[] args) {
 
-        string a="Hello,";
- string b="Git irnore"; 
-        system.out.print(result, a+b)
+     String str1 = "Hello, ";
+String str2 = "World!";
+System.out.print(str1 + str2);
 // remove comments and add your code here
     }
 }
