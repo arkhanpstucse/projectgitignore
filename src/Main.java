2ca8aefd3c282733bd7be61c6483f5f49c1ce819
arkhanpstucse <ar.ap@nbr.gov.bd>
2024-11-05 16:14:49 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class PracticeGitIgnore {
     public static void main(String[] args) {
+
+        string a="Hello,";
+ string b="Git irnore"; 
+        system.out.print(result, a+b)
 // remove comments and add your code here
     }
 }
